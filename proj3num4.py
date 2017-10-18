6fea9f3e818f0978887559c79de21ad2341dd6ea
Andre Mendoza <ubuntu@ip-172-31-16-146.us-west-2.compute.internal>
2017-10-18 21:16:29 +0000

    Find all 3XX errors

---
+++ proj3num4.py
@@ -0,0 +1,27 @@
+count = 0.0
+
+three_errors = 0.0
+
+percent_error = 0.0
+
+with open('http_access_log') as f:
+  for line in f:
+      #print line
+      if 'GET' in line:
+         count = count + 1
+
+three_string = '" 3'
+#The 3xx after the "GET xxxxxx" part of the line
+with open('http_access_log') as j:
+  for line in j:
+        if three_string in line:
+          three_errors = three_errors +1
+
+percent_error = three_errors/count *100
+
+
+print  "total requests made: ", count
+print  "total 4xx errors: ", three_errors
+print  "the % of 3xx of all requests:", percent_error, "%"
+
+
